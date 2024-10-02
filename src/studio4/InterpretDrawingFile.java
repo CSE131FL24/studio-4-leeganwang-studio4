8cf273f48f944e5d31b557f24290f87a0b5cf414
jessicalee <jessicalee@localhost>
2024-10-02 15:31:48 -0500

    Strawberry Matcha Latte Flag

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -20,5 +21,35 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledSquare(0.3, 0.7, 0.1);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledSquare(0.5, 0.7, 0.1);
+		
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledSquare(0.7, 0.7, 0.1);
+		
+		Color lime = new Color(163, 217, 128);
+		
+		StdDraw.setPenColor(lime);
+		StdDraw.filledSquare(0.3, 0.5, 0.1);
+		
+		StdDraw.setPenColor(lime);
+		StdDraw.filledSquare(0.5, 0.5, 0.1);
+		
+		StdDraw.setPenColor(lime);
+		StdDraw.filledSquare(0.7, 0.5, 0.1);
+		
+		Color darkgreen = new Color(1, 150, 39);
+
+		StdDraw.setPenColor(darkgreen);
+		StdDraw.filledSquare(0.3, 0.3, 0.1);
+		
+		StdDraw.setPenColor(darkgreen);
+		StdDraw.filledSquare(0.5, 0.3, 0.1);
+		
+		StdDraw.setPenColor(darkgreen);
+		StdDraw.filledSquare(0.7, 0.3, 0.1);
 	}
 }
